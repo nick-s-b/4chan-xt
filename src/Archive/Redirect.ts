9e429fae89f678964c4042d0c9234416010c8d30
Kalle Leskinen <kalle@leskinen.org>
2026-05-13 23:34:17 +0300

    Include Desuarchive in redirect formatting
    Added Desuarchive to timestamp length check to fix 404'd image redirects

--- src/Archive/Redirect.ts
+++ src/Archive/Redirect.ts
@@ -191,7 +191,7 @@ var Redirect = {
     } else {
       if (/[sm]\.jpg$/.test(filename)) { return ''; }
     }
-    if (archive.name.endsWith('arch.b4k.co') || archive.name.endsWith('palanq.win')) {
+    if (archive.name.endsWith('arch.b4k.co') || archive.name.endsWith('palanq.win') || archive.name.endsWith('Desuarchive')) {
       const [timeStamp, ext] = filename.split('.');
       if (timeStamp.length > 13) {
         // remove last 3 digits
